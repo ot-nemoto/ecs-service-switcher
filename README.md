d1663e8c28a2f04f3c9abb28c82c4f50266b2b8a
ot-nemoto <nemoto@opentone.co.jp>
2019-01-29 00:23:00 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# ecs-service-switcher
+
+### 概要
+
+- 指定された時間に、特定のタグが付与されたECSクラスタのFargateタスクを起動・停止する。
+- 起動する時間は平日（月～金）の午前８時。
+- 停止する時間は平日（月～金）の午後９時。
+- デフォルトのリージョンは東京（ap-northeast-1）。
+
+### 使い方
+
+- 起動・停止対象のECSクラスターに `ecs-service-switcher-isenabled` タグを付与。
+- タグの値には `ON`, `TRUE`, `1` の何れかを設定。
+- 起動・停止対象から外したい場合はタグを削除。またはタグの値に上記以外を設定。
+- 起動時デフォルトではタスク数は `1` 。
+- `default-desired-count` タグを指定した場合は、その値をタスク数に設定。
+
+### デプロイ
+
+*serverless*
+```sh
+(cd layer/nodejs; npm install)
+serverless deploy
+```
---
+++ ecs-services/stop.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+
+module.exports.handler = function(event, context) {
+  var ecs = new AWS.ECS();
+  var tagName = process.env['tag_name'];
+  var enabledValues = ['ON', 'On', 'on', 'TRUE', 'True', 'true', '1'];
+
+  ecs.listClusters({}, function(err, data) {
+    if (err) { console.log(err, err.stack); return }
+
+    data.clusterArns.forEach(function(clusterArn) {
+
+      ecs.listTagsForResource({ resourceArn: clusterArn }, function(err, data) {
+        if (err) { console.log(err, err.stack); return; }
+          
+        var tags = {};
+        data.tags.forEach(function(tag) { tags[tag.key] = tag.value; });
+        if (!tags[tagName] || !enabledValues.includes(tags[tagName])) return;
+        
+        var cluster = clusterArn.split("/")[1];
+        ecs.describeClusters({clusters: [cluster]}, function(err, data) {
+          if (err) { console.log(err, err.stack); return }
+
+          ecs.listServices({cluster: data.clusters[0].clusterName}, function(err, data) {
+            if (err) { console.log(err, err.stack); return }
+            
+            var service = clusterArn.split("/")[1];
+            ecs.updateService({cluster: cluster, service: service, desiredCount: 0}, function(err, data) {
+              if (err) console.log(err, err.stack);
+              else     console.log(data);
+            });
+          });
+        });
+      });
+    });
+  });
+};
