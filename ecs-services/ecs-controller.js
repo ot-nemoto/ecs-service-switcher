57669db9f8747d255348cb36c250c4db7aaee0b2
ot-nemoto <nemoto@opentone.co.jp>
2020-01-17 16:18:40 +0000

    modify #2
    - ログのコメントアウトを削除

--- ecs-services/ecs-controller.js
+++ ecs-services/ecs-controller.js
@@ -7,33 +7,28 @@ var EcsController = function(tagName) {
 
 EcsController.prototype.start = async function() {
   var ecs = new AWS.ECS();
-  
-  console.log(this.tagName);
-  
+
   var clusters = await ecs.listClusters({}).promise();
-  //console.log(clusters);
   for (var clusterArn of clusters.clusterArns) {
-    //console.log(clusterArn);
     var clasterTags = await ecs.listTagsForResource({resourceArn: clusterArn}).promise();
-    //console.log(clasterTags);
     var clusterSwitchValue = this.getTagValue(this.tagName, clasterTags.tags);
     var clusterDesiredCount = this.getTagValue('default-desired-count', clasterTags.tags);
     var services = await ecs.listServices({cluster: clusterArn}).promise();
-    //console.log(services);
+
     for (var serviceArn of services.serviceArns) {
       var serviceTags = await ecs.listTagsForResource({resourceArn: serviceArn}).promise();
-      //console.log(serviceTags);
       var serviceSwitchValue = this.getTagValue(this.tagName, serviceTags.tags);
       var serviceDesiredCount = this.getTagValue('default-desired-count', serviceTags.tags);
       var switchValue = serviceSwitchValue || clusterSwitchValue;
       var desiredCount = serviceDesiredCount || clusterDesiredCount || 1;
-      // debug
-      console.log(`ClusterSwitchValue: ${clusterSwitchValue}`);
-      console.log(`ServiceSwitchValue: ${serviceSwitchValue}`);
-      console.log(`SwitchValue: ${switchValue}`);
-      console.log(`ClusterDesiredCount: ${clusterDesiredCount}`);
-      console.log(`ServiceDesiredCount: ${serviceDesiredCount}`);
-      console.log(`DesiredCount: ${desiredCount}`);
+
+      console.debug(`ClusterSwitchValue: ${clusterSwitchValue}`);
+      console.debug(`ServiceSwitchValue: ${serviceSwitchValue}`);
+      console.debug(`SwitchValue: ${switchValue}`);
+      console.debug(`ClusterDesiredCount: ${clusterDesiredCount}`);
+      console.debug(`ServiceDesiredCount: ${serviceDesiredCount}`);
+      console.debug(`DesiredCount: ${desiredCount}`);
+
       if (!this.enabledValues.includes(switchValue)) continue;
       
       // UpdateService
@@ -56,25 +51,22 @@ EcsController.prototype.stop = async function() {
   console.log(this.tagName);
   
   var clusters = await ecs.listClusters({}).promise();
-  //console.log(clusters);
   for (var clusterArn of clusters.clusterArns) {
-    //console.log(clusterArn);
     var clasterTags = await ecs.listTagsForResource({resourceArn: clusterArn}).promise();
-    //console.log(clasterTags);
     var clusterSwitchValue = this.getTagValue(this.tagName, clasterTags.tags);
     var desiredCount = 0;
     var services = await ecs.listServices({cluster: clusterArn}).promise();
-    //console.log(services);
+
     for (var serviceArn of services.serviceArns) {
       var serviceTags = await ecs.listTagsForResource({resourceArn: serviceArn}).promise();
-      //console.log(serviceTags);
       var serviceSwitchValue = this.getTagValue(this.tagName, serviceTags.tags);
       var switchValue = serviceSwitchValue || clusterSwitchValue;
-      // debug
-      console.log(`ClusterSwitchValue: ${clusterSwitchValue}`);
-      console.log(`ServiceSwitchValue: ${serviceSwitchValue}`);
-      console.log(`SwitchValue: ${switchValue}`);
-      console.log(`DesiredCount: ${desiredCount}`);
+
+      console.debug(`ClusterSwitchValue: ${clusterSwitchValue}`);
+      console.debug(`ServiceSwitchValue: ${serviceSwitchValue}`);
+      console.debug(`SwitchValue: ${switchValue}`);
+      console.debug(`DesiredCount: ${desiredCount}`);
+
       if (!this.enabledValues.includes(switchValue)) continue;
       
       // UpdateService
